cada6c7f286932d155edcc3ae7a17ebc747c1aca
Benruby <benlevi1@gmail.com>
2017-08-11 17:43:11 +0300

    env prod setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,27 @@
-export const environment = {
-  production: true
+export const globals = {
+	anon_token: localStorage.getItem('anon_user_token'),
+	setAnonToken: function() {
+	environment.
+	token_auth_config.
+	globalOptions.
+	headers.
+	anon_user_token = this.anon_token;
+}
 };
+
+export const environment = {
+	production: true,
+	token_auth_config: {
+		apiBase: 'https://jst-be.herokuapp.com',
+		globalOptions: {
+			headers: {
+				'Content-Type':     'application/json',
+				'Accept':           'application/json',
+				"anon_user_token":  globals.anon_token
+			}
+		}
+
+	},
+};
+
+
