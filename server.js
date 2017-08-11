5a2fab9f2d8d06de55f25fe2d48605ab841f111d
Benruby <benlevi1@gmail.com>
2017-08-11 18:05:38 +0300

    env prod setup

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const path = require('path');
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
 // Run the app by serving the static files
 // in the dist directory
