c4d45f5e15daa79a50d96d2493bf629721da6213
Benruby <benlevi1@gmail.com>
2017-10-05 00:26:39 +0300

    prerenderReady in qa mode

--- src/app/q-and-a-mode/q-and-a-mode.component.ts
+++ src/app/q-and-a-mode/q-and-a-mode.component.ts
@@ -14,7 +14,7 @@ import {
 	import { SelectGameComponent } from './select-game/select-game.component';
 	import { SpinnerService } from '../components/spinner/spinner.service';
 	import { Title, Meta } from '@angular/platform-browser';
-	// import { WindowRef } from 'app/services/windowRef/window-ref';
+	import { WindowRef } from 'app/services/windowRef/window-ref';
 
 	declare var $ :any;
 
@@ -41,8 +41,8 @@ import {
 			private route: ActivatedRoute,
 			private spinnerService: SpinnerService,
 			private metaService: Meta,
-			private titleService: Title
-			// private winRef: WindowRef
+			private titleService: Title,
+			private winRef: WindowRef
 			) { }
 
 
@@ -50,38 +50,36 @@ import {
 			this.spinnerService.show();
 			this.gamesService.getGamesNames()
 			.then((res) => {
-					this.gamesNames = res.json().games;
-				}).then(() => {
-						this.titleService.setTitle("Web Questions - " + this.gameName);
-						// this.winRef.nativeWindow.prerenderReady = true
-					}
-					)
+				this.gamesNames = res.json().games;
+			});
 
-					this.gameNameParam = this.route.params.subscribe(params => {
-						this.gameName = params['game_name'];
-						this.getQuestionsForGame(this.gameName);		
-					});
-				}
+			this.gameNameParam = this.route.params.subscribe(params => {
+				this.gameName = params['game_name'];
+				this.getQuestionsForGame(this.gameName);		
+			});
+		}
 
-				ngAfterViewInit(): void {
-					this.quesCol.changes.subscribe(t => {
-						//init the collapsibles after game change.
-						$('.collapsible').collapsible();	
-					})
-				}
+		ngAfterViewInit(): void {
+			this.quesCol.changes.subscribe(t => {
+				//init the collapsibles after game change.
+				$('.collapsible').collapsible();
+				this.winRef.nativeWindow.prerenderReady = true
+				
+			})
+		}
 
-				getQuestionsForGame(gameName: string){
-					this.spinnerService.show();
-					this.questionsService.getGameQuestions(gameName)
-					.then((res)=> {
-						this.questions = res.json().questions;
-						this.selectGameDialog.closeDialog();
-						this.spinnerService.hide();
-					}).then(() => {
-						this.titleService.setTitle("Web Questions - " + this.gameName);
-						this.metaService.updateTag({ name: "description", content: this.questions[0].game_long_description});
-					});
-				}
+		getQuestionsForGame(gameName: string){
+			this.spinnerService.show();
+			this.questionsService.getGameQuestions(gameName)
+			.then((res)=> {
+				this.questions = res.json().questions;
+				this.selectGameDialog.closeDialog();
+				this.spinnerService.hide();
+			}).then(() => {
+				this.titleService.setTitle("Web Questions - " + this.gameName);
+				this.metaService.updateTag({ name: "description", content: this.questions[0].game_long_description});
+			});
+		}
 
 		/**
 		 * the method presents dialog for the user to select a game.
